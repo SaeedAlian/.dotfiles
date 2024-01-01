4eb6a65568733bb2cb01683294fca41ca8179764
SaeedAlian <aliansaeed82@gmail.com>
2024-01-01 22:04:41 +0330

    fix: cleanup nvim plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,7 +2,5 @@ return {
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",
 	"nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-web-devicons",
-  "nvim-treesitter/nvim-treesitter-context",
-  "nvim-treesitter/playground",
+	"nvim-tree/nvim-web-devicons",
 }
