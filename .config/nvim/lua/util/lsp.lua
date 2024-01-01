61da96c1f76c8306140bd99de1d3a12d80b98c69
SaeedAlian <aliansaeed82@gmail.com>
2024-01-01 22:08:19 +0330

    fix: nvim lsp keybindings

--- .config/nvim/lua/util/lsp.lua
+++ .config/nvim/lua/util/lsp.lua
@@ -5,7 +5,7 @@ local M = {}
 M.on_attach = function(_, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	map("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", "Show LSP references", opts)
+	map("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", "Show LSP references", opts)
 
 	map("n", "<leader>gd", vim.lsp.buf.definition, "Go to definition", opts)
 
