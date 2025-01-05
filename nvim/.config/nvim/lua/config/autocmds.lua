9fee6601bf589ea92014ea8142a4099bc785ce99
SaeedAlian <aliansaeed82@gmail.com>
2025-01-05 11:27:09 +0330

    update: nvim conf

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -3,8 +3,8 @@ vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format({ timeout_ms = 6000 })]])
 
 -- set textwidth to 80 for Markdown files
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown" },
-  callback = function()
-    vim.opt_local.textwidth = 80
-  end,
+	pattern = { "markdown" },
+	callback = function()
+		vim.opt_local.textwidth = 80
+	end,
 })
