074e131374a67e4cb6a334b2d924ff0a80fdb52d
SaeedAlian <aliansaeed82@gmail.com>
2025-01-07 03:34:04 +0330

    update: polybar conf

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,6 +2,7 @@
 
 # kill all polybar instances
 killall -q polybar
+rm /tmp/polybarshow
 
 # wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
@@ -10,5 +11,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 echo "---" | tee -a /tmp/polybar.log
 polybar bar 2>&1 | tee -a /tmp/polybar.log &
 disown
+touch /tmp/polybarshow
 
 echo "Bars launched..."
