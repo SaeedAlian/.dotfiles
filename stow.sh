74d551588dc54b12abc1704e48fe88d08ac27299
SaeedAlian <aliansaeed82@gmail.com>
2025-01-11 18:51:33 +0330

    update: stow script

--- stow.sh
+++ stow.sh
@@ -1,51 +1,55 @@
-#!/bin/bash
+#!/bin/sh
 
-use_zsh=1
+use_zsh=0
+use_bash=1
 use_i3=0
 use_bspwm=1
 use_alacritty=1
 
 while [ $# -gt 0 ]; do
-  case $1 in
-  --shell)
-    use_zsh=0
-    shift 1
-    if [ "$1" == "zsh" ]; then
-      use_zsh=1
-    else
-      echo "Wrong shell entered" >&2
-      exit 1
-    fi
-    ;;
-  --wm)
-    use_bspwm=0
-    use_i3=0
-    shift 1
-    if [ "$1" == "bspwm" ]; then
-      use_bspwm=1
-    elif [ "$1" == "i3" ]; then
-      use_i3=1
-    else
-      echo "Wrong wm entered" >&2
-      exit 1
-    fi
-    ;;
-  --terminal)
-    use_alacritty=0
-    shift 1
-    if [ "$1" == "alacritty" ]; then
-      use_alacritty=1
-    else
-      echo "Wrong terminal entered" >&2
-      exit 1
-    fi
-    ;;
-  *)
-    echo "Wrong option $1" >&2
-    exit 1
-    ;;
-  esac
-  shift 1
+	case $1 in
+	--shell)
+		use_zsh=0
+		use_bash=0
+		shift 1
+		if [ "$1" == "zsh" ]; then
+			use_zsh=1
+		elif [ "$1" == "bash" ]; then
+			use_bash=1
+		else
+			echo "Wrong shell entered" >&2
+			exit 1
+		fi
+		;;
+	--wm)
+		use_bspwm=0
+		use_i3=0
+		shift 1
+		if [ "$1" == "bspwm" ]; then
+			use_bspwm=1
+		elif [ "$1" == "i3" ]; then
+			use_i3=1
+		else
+			echo "Wrong wm entered" >&2
+			exit 1
+		fi
+		;;
+	--terminal)
+		use_alacritty=0
+		shift 1
+		if [ "$1" == "alacritty" ]; then
+			use_alacritty=1
+		else
+			echo "Wrong terminal entered" >&2
+			exit 1
+		fi
+		;;
+	*)
+		echo "Wrong option $1" >&2
+		exit 1
+		;;
+	esac
+	shift 1
 done
 
 # stow defaults
@@ -66,20 +70,24 @@ stow yazi
 stow zathura
 
 if [ $use_alacritty == 1 ]; then
-  stow alacritty
+	stow alacritty
 fi
 
 if [ $use_bspwm == 1 ]; then
-  stow bspwm
-  stow sxhkd
+	stow bspwm
+	stow sxhkd
 fi
 
 if [ $use_i3 == 1 ]; then
-  stow i3
+	stow i3
 fi
 
 if [ $use_zsh == 1 ]; then
-  stow zsh
+	stow zsh
+fi
+
+if [ $use_bash == 1 ]; then
+	stow bash
 fi
 
 exit 0
