bfafceda66a9563107dbc192e066121370cea408
SaeedAlian <aliansaeed82@gmail.com>
2026-06-24 20:24:10 +0330

    fix: remove array usage in stow.sh

--- stow.sh
+++ stow.sh
@@ -8,7 +8,7 @@ require_vars \
   XDG_DATA_HOME
 
 cd "$DOTFILES_DIR"
-STOW_FLAGS=()
+STOW_FLAGS=""
 
 mkdir -p $HOME/.themes
 mkdir -p $SCRIPTS_DIR
@@ -40,25 +40,26 @@ mkdir -p $XDG_CONFIG_HOME/qt5ct/colors
 mkdir -p $XDG_CONFIG_HOME/qt6ct/colors
 
 # stow defaults
-stow "${STOW_FLAGS[@]}" X11
-stow "${STOW_FLAGS[@]}" alacritty
-stow "${STOW_FLAGS[@]}" bash
-stow "${STOW_FLAGS[@]}" bspwm
-stow "${STOW_FLAGS[@]}" dockerscripts
-stow "${STOW_FLAGS[@]}" dunst
-stow "${STOW_FLAGS[@]}" fastfetch
-stow "${STOW_FLAGS[@]}" fonts
-stow "${STOW_FLAGS[@]}" git
-stow "${STOW_FLAGS[@]}" mpv
-stow "${STOW_FLAGS[@]}" nvim
-stow "${STOW_FLAGS[@]}" picom
-stow "${STOW_FLAGS[@]}" polybar
-stow "${STOW_FLAGS[@]}" redshift
-stow "${STOW_FLAGS[@]}" rofi
-stow "${STOW_FLAGS[@]}" scripts
-stow "${STOW_FLAGS[@]}" sxhkd
-stow "${STOW_FLAGS[@]}" sxiv
-stow "${STOW_FLAGS[@]}" tmux
-stow "${STOW_FLAGS[@]}" userconf
-stow "${STOW_FLAGS[@]}" yazi
-stow "${STOW_FLAGS[@]}" zathura
+stow $STOW_FLAGS X11
+stow $STOW_FLAGS alacritty
+stow $STOW_FLAGS bash
+stow $STOW_FLAGS bspwm
+stow $STOW_FLAGS dconf
+stow $STOW_FLAGS dockerscripts
+stow $STOW_FLAGS dunst
+stow $STOW_FLAGS fastfetch
+stow $STOW_FLAGS fonts
+stow $STOW_FLAGS git
+stow $STOW_FLAGS mpv
+stow $STOW_FLAGS nvim
+stow $STOW_FLAGS picom
+stow $STOW_FLAGS polybar
+stow $STOW_FLAGS redshift
+stow $STOW_FLAGS rofi
+stow $STOW_FLAGS scripts
+stow $STOW_FLAGS sxhkd
+stow $STOW_FLAGS sxiv
+stow $STOW_FLAGS tmux
+stow $STOW_FLAGS userconf
+stow $STOW_FLAGS yazi
+stow $STOW_FLAGS zathura
